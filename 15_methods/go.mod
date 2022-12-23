c849a86eb6ecb7bb34927bfce00f2f70c98ed29e
Vinay Chand Chunchu <vinaychunchu@Vinays-Air.attlocal.net>
2022-12-23 09:35:50 -0600

    GO Bascis first two

---
+++ 15_methods/go.mod
@@ -0,0 +1,3 @@
+module github.com/VinayChunchu/GoLearning/methods
+
+go 1.19
---
+++ 15_methods/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Methods learning")
+
+	// There is no inheritence in GO lang. No super or parent as well.
+	vinay := User{"VinayChunchu", "vinay@vinaychunchu.com", true, 31}
+	fmt.Println(vinay)
+	// %+v is used to get the property name of the struct printed out.
+	fmt.Printf("Details of user vinay are : %+v \n", vinay)
+
+	fmt.Printf("Name : %v \nEmail : %v \n", vinay.Name, vinay.Email)
+
+	vinay.GetStatus()
+	vinay.NewMail()
+
+	fmt.Printf("Name : %v \nEmail : %v \n", vinay.Name, vinay.Email)
+
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func (user User) GetStatus() {
+	fmt.Println("Status of the user is : ", user.Status)
+}
+
+// When structs are passed to methods, a copy is passed NOT the actual struct.
+// This is another case where we use pointers
+func (user User) NewMail() {
+	user.Email = "chand@VinayChunchu.com"
+	fmt.Println("New email of the user: ", user.Email)
+}
